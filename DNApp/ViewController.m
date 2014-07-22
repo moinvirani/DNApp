b78c7f8acbdd0a0bde0e999a7c963bd148923a4f
Moin Virani <moin.virani@gmail.com>
2014-07-22 19:56:14 +0400

    first commit

--- DNApp/ViewController.m
+++ DNApp/ViewController.m
@@ -17,13 +17,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// Do any additional setup after loading the view.
+    
+    [self setNeedsStatusBarAppearanceUpdate];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
 }
 
 @end
